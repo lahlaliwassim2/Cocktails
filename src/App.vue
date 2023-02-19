abafaeb95173737e013a3c2dcdeae12a60a48d64
lahlaliwassim2 <wlahlali34@gmail.com>
2023-02-19 15:20:37 +0100

    preparation de l'envirenement en supprimant les fichier qui viennent avec le projet initial

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,6 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
+      <img alt="Vue logo" src="./assets/logo.png">
+
   <router-view/>
 </template>
 
