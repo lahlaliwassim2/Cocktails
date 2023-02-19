3f50d86582cfef4243ee1adc74e2b93ab525516e
lahlaliwassim2 <wlahlali34@gmail.com>
2023-02-19 17:12:30 +0100

    ajouter les router link de mes trois pages

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,10 @@
 <template>
       <img alt="Vue logo" src="./assets/logo.png">
+      <nav>
+        <router-link to='/'>Home</router-link> |
+        <router-link to='/Contact'>Contact</router-link> |
+        <router-link to='/About'>About</router-link> |
+      </nav>
   <router-view/>
 </template>
 
